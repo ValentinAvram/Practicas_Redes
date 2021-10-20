cc9e064ab2ea5c5cbb9b98c5cc9117017ea85cc8
Valentín <valentingabrielavram@gmail.com>
2021-10-20 14:01:43 +0000

    TODOs archivos usuarios

--- usuario.cpp
+++ usuario.cpp
@@ -4,5 +4,21 @@ using namespace std;
 
 //TODO: Añadir aqui las funciones de la clase usuario
 //Añadir constructor
+Usuario::Usuario(int descriptor)
+{
+    sd = descriptor;
+    estado = 0;
+
+    checkNombre = false;
+    checkPass = false;
+}
 
 //TODO: Funcion de comprobar login
+bool Usuario::checkLogin()
+{
+    if(checkNombre == true && checkPass == true)
+    {
+        return true;
+    }
+    return false;
+}
--- usuario.h
+++ usuario.h
@@ -13,30 +13,24 @@ class Usuario{
     int sd;
     char nombre[250];
     char password[250];
-    int estado;
-    //Status 0 en cola, 1 jugando
+    int estado; //Status 0 en cola, 1 jugando
+    
     int sdRival;
     int idJuego;
 
     bool checkNombre;
     bool checkPass;
 
-    bool 
     public:
-
-    User(){
-        sd=-1;
-        estado = 0;
-    }
-
+    
     inline int getSd() {return sd;}
     inline char *getNombre() {return nombre;}
     inline char * getPassword() {return password;}
     inline int getStatus() {return estado;}
 
     inline void setSd(int Sd) { sd = Sd;}
-    inline void setNombre(char *Nombre) {strncpy(nombre,Nombre,250);} // TODO: Es necesario el strncpy
+    inline void setNombre(char *Nombre) {strncpy(nombre,Nombre,250);} //Es necesario el strncpy??
     //...
-    inline void setPassword(char *Password) { strncpy(password,Password, 250);}
+    inline void setPassword(char *Password) { strncpy(password,Password, 250);} //Es necesario el strncpy??
     inline void setStatus(int Estado) {estado=Estado;}
 };
